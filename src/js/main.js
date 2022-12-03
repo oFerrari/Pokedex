7d397b058cc4f660c1259dde76652454a79968f1
Andrei Ferrari <andrei.domingos@estudante.ifms.edu.br>
2022-12-02 22:56:33 -0400

    Commit aula 01

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,33 @@
 import '../css/style.css'
+
+/* Selecionar Elementos no DOM */
+
+const inputPesquisa = document.querySelector("#inputPesquisa")
+const btnLocalizar = document.querySelector("#btnLocalizar")
+const pokedexDisplay = document.querySelector("#display")
+
+/* Adicionando Eventos */
+
+btnLocalizar.addEventListener('click', async () => {
+    /* Buscar os dados do pokemon na API */
+    const dadosDoPokemon = await localizarPokemon(inputPesquisa.value)
+    /* Criar o cartao do pokemon */
+    criarCartao(dadosDoPokemon)
+})
+
+async function localizarPokemon(termoBusca){
+    const url = `https://pokeapi.co/api/v2/pokemon/${termoBusca}`
+    const response = await fetch(url)
+    const pokemon = await response.json();
+    return pokemon
+}
+
+function criarCartao(pokemon){
+    const cartaoPokemon = document.createElement('div')
+    cartaoPokemon.className = 'cartaoPokemon'
+    cartaoPokemon.innerHTML = `
+        <img class="pokemonSprite" src="${pokemon.sprites.front_default}"/>
+        <h2>${pokemon.name}</h2>
+    `
+    pokedexDisplay.appendChild(cartaoPokemon)
+}
